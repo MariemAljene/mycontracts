75b5bad9dd9802a052e67e46fcad6217e27cb142
mimi667 <mariem.aljene@esprit.tn>
2024-01-07 11:10:44 +0100

    CalculationTrust

--- contracts/CSPFeedbackModule.sol
+++ contracts/CSPFeedbackModule.sol
@@ -7,23 +7,30 @@ contract CSPFeedbackModule {
     // Mapping to store a list of feedbacks for each customer and CSP pair
     mapping(address => mapping(address => uint256[])) public cspFeedback;
 
+    // Mapping to store the trust score for each CSP
+    mapping(address => uint256) public cspTrustScore;
+
+    // Event to emit when feedback is received
     event FeedbackReceived(address indexed csp, address indexed customer, uint256 rating);
 
     constructor() {
         owner = msg.sender;
     }
 
+    // Modifier to restrict access to the owner
     modifier onlyOwner() {
         require(msg.sender == owner, "Permission denied");
         _;
     }
 
+    // Function to submit feedback
     function submitFeedback(address customer, uint256 rating) public {
         require(rating >= 1 && rating <= 5, "Invalid rating");
 
         // Store the feedback in the array
         cspFeedback[msg.sender][customer].push(rating);
 
+        // Emit the feedback received event
         emit FeedbackReceived(msg.sender, customer, rating);
     }
 
@@ -43,5 +50,30 @@ contract CSPFeedbackModule {
         } else {
             return 0; // Return 0 if there are no feedbacks
         }
+    }
+
+    // Function to calculate the trust score for a CSP
+    function calculateTrustScore(address csp) internal view returns (uint256) {
+        uint256 averageFeedback = getAverageFeedback(csp);
+        
+        // Formula for trust calculation
+        uint256 tau = 1;
+        uint256 m = cspFeedback[msg.sender][csp].length / (cspFeedback[msg.sender][csp].length + tau);
+
+        uint256 trustScore = averageFeedback + (m * tau) / 2;
+        return trustScore;
+    }
+
+    // Function to update the trust score for a CSP
+    function updateTrustScore(address csp) public onlyOwner {
+        uint256 newTrustScore = calculateTrustScore(csp);
+
+        // Update the trust score
+        cspTrustScore[csp] = newTrustScore;
+    }
+
+    // Function to get the trust score for a CSP
+    function getTrustScore(address csp) public view returns (uint256) {
+        return cspTrustScore[csp];
     }
 }
